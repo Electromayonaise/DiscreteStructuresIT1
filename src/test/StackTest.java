f96d17d68c5045398b4497965a938570cf86dba6
jul109 <115905294+jul109@users.noreply.github.com>
2023-09-29 19:44:30 -0500

    stackAndQueueTests

---
+++ src/test/StackTest.java
@@ -0,0 +1,78 @@
+
+import model.DoublyLinkedList;
+import model.Stack;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackTest {
+    private Stack<Integer> stack;
+    private int NUM_ELEMENTS =100;
+
+    public void setUpEmptyStack(){ // Cr
+        stack=new DoublyLinkedList<>();
+    }
+
+    public void setUpStackWithElements(){
+        setUpEmptyStack();
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            stack.push(i);
+        }
+    }
+
+
+
+
+    @Test
+
+    public void testPop(){
+        setUpStackWithElements();
+        for (int i = NUM_ELEMENTS-1; i >=0 ; i--) {
+            assertEquals(i,stack.pop());
+        }
+    }
+
+    @Test
+    public void testTop(){
+        setUpEmptyStack();
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            stack.push(i);
+            stack.push(i+1);
+            assertEquals(stack.top(),i+1);
+        }
+    }
+
+
+
+    @Test
+
+    public void testSize(){
+        setUpStackWithElements();
+        int size= NUM_ELEMENTS;
+        while(size>=0){
+            assertEquals(size,stack.size());
+            stack.pop();
+            size--;
+        }
+        stack.pop();
+        assertEquals(0,stack.size());
+
+    }
+    @Test
+    public void testEmpty(){
+        setUpStackWithElements();
+        int size=NUM_ELEMENTS;
+        while(size>=0){
+            if(size!=0){
+                assertFalse(stack.empty());
+            }else{
+                assertTrue(stack.empty());
+            }
+            stack.pop();
+            size--;
+
+        }
+    }
+
+}

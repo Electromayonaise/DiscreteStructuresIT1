4755599c18b6b2682ff39b799ab97fec98dd3cc8
jul109 <115905294+jul109@users.noreply.github.com>
2023-10-03 12:00:22 -0500

    clasS Task Creation

---
+++ src/model/Task.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.Arrays;
+
+public class Task {
+    private String title;
+    private String description;
+    private int priorityLevel;
+
+    public Task(String title, String description, int priorityLevel) {
+        this.title = title;
+        this.description = description;
+        this.priorityLevel = priorityLevel;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriorityLevel() {
+        return priorityLevel;
+    }
+     
+    public void setPriorityLevel(int priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+    @Override
+    public String toString()   {
+        return "title:"+title+ " description:"+description+" priorityLevel: "+priorityLevel;
+    }
+
+
+    public String[] getAttributes(){
+        String[] attributes= new String[3];
+        attributes[0]=title;
+        attributes[1]=description;
+        attributes[2]=priorityLevel+"";
+        return Arrays.copyOf(attributes,attributes.length);
+    }
+}
